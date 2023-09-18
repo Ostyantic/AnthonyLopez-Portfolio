3aa5b1051df765aca7e7b58cf2caa1970ca1e48d
Anthony Lopez <Antl21292@gmail.com>
2023-09-17 21:02:17 -0400

    about, project, contact views added

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,15 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+
+class ProjectPageView(TemplateView):
+    template_name = 'projects.html'
