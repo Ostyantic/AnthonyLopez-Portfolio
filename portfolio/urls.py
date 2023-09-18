1f50b27da11fa98689516c2d15e498ebf9419f1a
Anthony Lopez <Antl21292@gmail.com>
2023-09-17 21:02:07 -0400

    about, project, contact urls added

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AboutPageView, ContactPageView, ProjectPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('about', AboutPageView.as_view(), name='about'),
+    path('projects', ProjectPageView.as_view(), name='projects'),
+    path('contactme', ContactPageView.as_view(), name='contact'),
 ]
