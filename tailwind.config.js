84e047ca6835e3212cf6b35ff42964d26292bc39
Anthony Lopez <Antl21292@gmail.com>
2023-09-28 19:18:00 -0400

    Added some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
       './node_modules/flowbite/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+           'Lumigreen': '#00FD38',
+            'Mossy': '#3F4A3C',
+            'Sage': '#A2AF9F',
+            'Aqualight': '#00E1FF',
+            'Cerulean': '#00A8FF',
+        },
+    },
   },
   plugins: [
       require('flowbite/plugin'),
